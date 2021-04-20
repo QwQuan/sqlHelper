85e8bfd3c3a8df5e20d47583b14c36ce43e1d235
QwQuan <53105341+QwQuan@users.noreply.github.com>
2021-04-20 20:08:46 +0800

    Update SqlUtil.java

--- src/util/SqlUtil.java
+++ src/util/SqlUtil.java
@@ -13,11 +13,6 @@ public class SqlUtil {
         return sql;
     }
 
-//    public static void main(String[] args) {
-//        String parse = parse("SELECT wwp.WORK_PLAN_ID,  wwp.PRE_WORK_PLAN_ID,  wwp.SF_SORT,  wwp.WORK_SORT,  wwp.WORK_SORT_SUB,  wwp.PLAN_NO,  wwp.PLAN_TYPE,  wwp.PLAN_CYCLE,  wwp.PLAN_YEAR,  wwp.PLAN_MONTH,  wwp.ORG_NO,  wwp.DEPT_NO,  wwp.EMP_NO,  wwp.MAKE_DATE,  wwp.PLAN_STATUS,  wwp.PLAN_NUM,  wwp.FINISH_NUM,  wwp.EXCEPT_NUM,  wwp.APP_NO,  wwp.REMARK,  wwp.PLAN_WEEK,  wwp.PLAN_CONTENT,  wwp.PLAN_SEASON,\twwpd.WPD_ID,  wwpd.OBJ_ID,  wwpd.OBJ_TYPE,  wwpd.VOLT_CODE,  wwpd.MNGN_ORG_NO,  wwpd.MNTN_ORG_NO,  wwpd.ADDR,  wwpd.PLAN_DATE,  wwpd.MAKE_ORG_NO,  wwpd.MAKE_DEPT_NO,  wwpd.MAKE_EMP_NO,  wwpd.EXE_ORG_NO,  wwpd.EXE_DEPT_NO,  wwpd.EXE_EMP_NO,  wwpd.EXE_DATE,  wwpd.COM_RESON,  wwpd.SRC_WPD_ID,  wwpd.TASK_NO FROM w_work_plan_det wwpd , w_work_plan wwp where wwpd.WORK_PLAN_ID=wwp.WORK_PLAN_ID");
-//        System.out.println(parse);
-//    }
-
     public static String parse(String sql){
         String clearLowSql = clearSql(toSqlLowerCase(sql));
         String[] strings = cutSql(clearLowSql);
